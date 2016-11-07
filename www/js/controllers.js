25ee82765dfdbd15178705e11df3264c55012f17
marcosocon <oconmarcos97@gmail.com>
2016-11-06 23:58:42 -0300

    replaced spaces for tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -51,99 +51,98 @@ angular.module('starter.controllers', ['lodash'])
 })
 
 .controller('ActivityCtrl', function($scope , $ionicModal, $ionicPopup) {
-    $scope.moves = localStorage.getItem("moves") ? JSON.parse(localStorage.getItem("moves")) : [];
-    $scope.newMove = {};
+	$scope.moves = localStorage.getItem("moves") ? JSON.parse(localStorage.getItem("moves")) : [];
+	$scope.newMove = {};
 
-    $scope.addMove = function(newMove){
-        if (newMove && _.isNumber(newMove.amount) && newMove.currency && newMove.type) {
-            $scope.moves.push(newMove);
-            $scope.closeModal();
+	$scope.addMove = function(newMove){
+		if (newMove && _.isNumber(newMove.amount) && newMove.currency && newMove.type) {
+			$scope.moves.push(newMove);
+			$scope.closeModal();
 			localStorage.setItem("moves", JSON.stringify($scope.moves));
 			$scope.getPesosBalance();
 			$scope.getDollarsBalance();
-        } else {
+		} else {
 			$ionicPopup.alert({
 				title: 'Error',
 				template: 'Por favor complete los campos.'
 			});
 			return false;
 		}
-    };
+	};
 
-    $scope.getPesosBalance = function(){
-      $scope.totalPesosIncome = 0;
-      $scope.totalPesosOutcome = 0;
-      var moneyPlus = _.filter($scope.moves, {'type': 'Ingreso', 'currency': 'Pesos'});
-      _.forEach(moneyPlus, function(move){
-        $scope.totalPesosIncome += move.amount;
-      });
-      var moneyMinus = _.filter($scope.moves, {'type': 'Egreso', 'currency': 'Pesos'});
-      _.forEach(moneyMinus, function(move){
-        $scope.totalPesosOutcome += move.amount;
-      });
-    };
+	$scope.getPesosBalance = function(){
+		$scope.totalPesosIncome = 0;
+		$scope.totalPesosOutcome = 0;
+		var moneyPlus = _.filter($scope.moves, {'type': 'Ingreso', 'currency': 'Pesos'});
+		_.forEach(moneyPlus, function(move){
+			$scope.totalPesosIncome += move.amount;
+		});
+		var moneyMinus = _.filter($scope.moves, {'type': 'Egreso', 'currency': 'Pesos'});
+		_.forEach(moneyMinus, function(move){
+			$scope.totalPesosOutcome += move.amount;
+		});
+	};
 
-    $scope.getDollarsBalance = function(){
-      $scope.totalDollarsIncome = 0;
-      $scope.totalDollarsOutcome = 0;
-      var moneyPlus = _.filter($scope.moves, {'type': 'Ingreso', 'currency': 'Dolares'});
-      _.forEach(moneyPlus, function(move){
-        $scope.totalDollarsIncome += move.amount;
-      });
-      var moneyMinus = _.filter($scope.moves, {'type': 'Egreso', 'currency': 'Dolares'});
-      _.forEach(moneyMinus, function(move){
-        $scope.totalDollarsOutcome += move.amount;
-      });
-    };
+	$scope.getDollarsBalance = function(){
+		$scope.totalDollarsIncome = 0;
+		$scope.totalDollarsOutcome = 0;
+		var moneyPlus = _.filter($scope.moves, {'type': 'Ingreso', 'currency': 'Dolares'});
+		_.forEach(moneyPlus, function(move){
+			$scope.totalDollarsIncome += move.amount;
+		});
+		var moneyMinus = _.filter($scope.moves, {'type': 'Egreso', 'currency': 'Dolares'});
+		_.forEach(moneyMinus, function(move){
+			$scope.totalDollarsOutcome += move.amount;
+		});
+	};
 
-    $ionicModal.fromTemplateUrl('templates/modal.html', {
-        scope: $scope
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
-    $scope.openModal = function() {
-        $scope.newMove = {};
-        $scope.modal.show();
-    };
-    $scope.closeModal = function() {
-        $scope.modal.hide();
-    };
+	$ionicModal.fromTemplateUrl('templates/modal.html', {
+		scope: $scope
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	$scope.openModal = function() {
+		$scope.newMove = {};
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
 
-    $scope.remove = function(move){
-        removeByAttr($scope.moves, '$$hashKey', move.$$hashKey);
-		$scope.moves = localStorage.getItem("moves");
-        $scope.getPesosBalance();
-        $scope.getDollarsBalance();
-    };
+	$scope.remove = function(move){
+		removeByAttr($scope.moves, '$$hashKey', move.$$hashKey);
+		localStorage.setItem("moves", JSON.stringify($scope.moves));
+		$scope.getPesosBalance();
+		$scope.getDollarsBalance();
+	};
 
-    $scope.getPesosBalance();
-    $scope.getDollarsBalance();
-
-    var removeByAttr = function(arr, attr, value){
-    var i = arr.length;
-    while(i--){
-        if( arr[i] && arr[i].hasOwnProperty(attr) && (arguments.length > 2 && arr[i][attr] === value ) ){
-            arr.splice(i,1);
-        }
-    }
-    return arr;
-    };
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	$scope.getPesosBalance();
+	$scope.getDollarsBalance();
+	var removeByAttr = function(arr, attr, value){
+		var i = arr.length;
+		while(i--){
+			if( arr[i] && arr[i].hasOwnProperty(attr) && (arguments.length > 2 && arr[i][attr] === value ) ){
+				arr.splice(i,1);
+			}
+		}
+		return arr;
+	};
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
 
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+	$scope.settings = {
+		enableFriends: true
+	};
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,31 +1,31 @@
 angular
-     .module('starter.services', [])
-     .factory('dataService', dataService);
+	.module('starter.services', [])
+	.factory('dataService', dataService);
 
 angular
-    .module('lodash', [])
-    .factory('_', ['$window', function($window) {
-      return $window._; // assumes underscore has already been loaded on the page
-    }]);
+	.module('lodash', [])
+	.factory('_', ['$window', function($window) {
+		return $window._; // assumes underscore has already been loaded on the page
+	}]);
 
- dataService.$inject = ['$http'];
+	dataService.$inject = ['$http'];
 
- function dataService($http) {
-     return {
-         getData: getData
-     };
+	function dataService($http) {
+		return {
+			getData: getData
+		};
 
-     function getData() {
-         return $http.get('http://ws.geeklab.com.ar/dolar/get-dolar-json.php')
-             .then(getDataComplete)
-             .catch(getDataFailed);
+		function getData() {
+			return $http.get('http://ws.geeklab.com.ar/dolar/get-dolar-json.php')
+			.then(getDataComplete)
+			.catch(getDataFailed);
 
-         function getDataComplete(response) {
-             return response.data;
-         }
+			function getDataComplete(response) {
+				return response.data;
+			}
 
-         function getDataFailed(error) {
-             console.log('XHR Failed for getData.' + error.data);
-         }
-     }
- }
+			function getDataFailed(error) {
+				console.log('XHR Failed for getData.' + error.data);
+			}
+		}
+	}
